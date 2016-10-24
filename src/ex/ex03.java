660b63f27f64da2922371a0039da4f8d9aebd9d0
Yingyin <105791120@live.asia.edu.tw>
2016-10-24 15:46:11 +0800

    ex03 105791120 杨钰莹

--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -1,15 +1,29 @@
 ﻿package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 讓使用者輸入一正整數 n，並輸出 2^1+2^2+2^3+...+2^n 的值。
  * Date: 2016/10/24
- * Author: 1050210XX 周永振老師
+ * Author: 105791120 楊鈺瑩
  */
 
 public class ex03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner sca=new Scanner(System.in);
+	int n=sca.nextInt();
+	long sum=0,temp;
+	for(int i=1;i<=n;i++)
+	{
+		temp=1;
+		for(int j=1;j<=i;j++)
+		{
+			temp*=2;
+		}
+		sum+=temp;
 	}
-
+	System.out.println(sum);
+	}
 }
+
