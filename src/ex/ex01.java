1e2df60fd7f345ee53a3ac53def55dc607f8c0ae
Yingyin <105791120@live.asia.edu.tw>
2016-10-24 15:45:03 +0800

    ex01 105791120 杨钰莹

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -1,15 +1,24 @@
 ﻿package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 讓使用者輸入一正整數n，用for迴圈計算n!後輸出。
  * Date: 2016/10/24
- * Author: 1050210XX 周永振老師
+ * Author: 105791120 楊鈺瑩
  */
 
 public class ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner sca = new Scanner(System.in);
+	int n=sca.nextInt();
+	long sum=1;
+	for(int i=1;i<=n;i++)
+	{
+		sum*=i;
+	}
+	System.out.println(sum);
 	}
 
 }
