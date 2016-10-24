4236fe39e20399c6c3ab0e2206814f8536cfc2a4
Yingyin <105791120@live.asia.edu.tw>
2016-10-24 15:46:33 +0800

    ex04 105791120 杨钰莹

--- src/ex/ex04.java
+++ src/ex/ex04.java
@@ -1,15 +1,37 @@
 ﻿package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 讓使用者輸入一正整數 n，以字元 '*' 輸出邊長為 n 的正方形 (請利用迴圈，不要直接用 printf 方式直接輸出)。
  * Date: 2016/10/24
- * Author: 1050210XX 周永振老師
+ * Author: 105791120 楊鈺瑩
  */
 
 public class ex04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner sca= new Scanner(System.in);
+	int n=sca.nextInt();
+	String str="*";
+    for(int i=0;i<n;i++)
+    {
+    	str="*";
+    	if (i==0||i==n-1)
+    	{
+    		for(int j=1;j<n-1;j++)
+    			str+="*";
+    	}
+    	else 
+    	{
+    		for(int j=1;j<n-1;j++)
+    		{
+    			str+="*";
+    		}
+    	}
+    	str+="*";
+    	System.out.println(str);
+    }
 	}
 
 }
