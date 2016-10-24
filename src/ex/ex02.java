4f9a386eb0da837ed180297fbb5e0782ef4336a1
Yingyin <105791120@live.asia.edu.tw>
2016-10-24 15:45:40 +0800

    ex02 105791120 杨钰莹

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,15 +1,25 @@
 ﻿package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 讓使用者輸入一正整數n，用while迴圈計算n!後輸出。
  * Date: 2016/10/24
- * Author: 1050210XX 周永振老師
+ * Author: 105791120 楊鈺瑩
  */
 
 public class ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner sca= new Scanner(System.in);
+	int n=sca.nextInt();
+	long sum=1;
+	while(n!=0)
+	{
+		sum*=n;
+		n--;
+	}
+   System.out.println(sum);
 	}
 
 }
