c25bdb726ca5c83e54a93534736535fefb55927c
Yingyin <105791120@live.asia.edu.tw>
2016-10-24 16:07:12 +0800

    ex06 105791120 杨钰莹

--- src/ex/ex06.java
+++ src/ex/ex06.java
@@ -1,4 +1,7 @@
 ﻿package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 某個陣列中含有所有學生的學號，但並沒有按照大小次序排列。請寫一程式依學號大小排列並將結果輸出。
  * Date: 2016/10/24
@@ -8,8 +11,25 @@
 public class ex06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner sca=new Scanner(System.in);
+	int data[]={105791180,105791123,105791129,105791190,10579145,10579110};
+    int a=0;
+    for(int i=0;i<data.length;i++)
+    {
+    	for(int j=0;j<data.length;j++)
+    	{
+    		if(data[i]<data[j])
+    		{
+    			a=data[i];
+    			data[i]=data[j];
+    			data[j]=a;
+    		}
+    	}
+    }
+    for(int i=0;i<data.length;i++)
+    {
+    	System.out.print(data[i]+"\t");
+    }
 	}
 
 }
