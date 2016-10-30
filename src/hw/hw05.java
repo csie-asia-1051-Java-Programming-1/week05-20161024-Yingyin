741adf9073439a69440f247bf35d2c7c2935c992
Yingyin <105791120@live.asia.edu.tw>
2016-10-30 15:03:51 +0800

    hw05 105791120 杨钰莹

--- src/hw/hw05.java
+++ src/hw/hw05.java
@@ -1,10 +1,42 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner sca=new Scanner(System.in);
+	int n=sca.nextInt();
+	int arr[][]=new int[21][21];
+	int num=1,p,q;
+	for(int i=1;i<=n;i++)
+	{
+		p=i;
+		for(int j=1;j<i &&p>0;j++)
+		{
+			arr[p][j]=num++;
+			p--;
+		}
+	}
+	int flag=1;
+	for(int i=2;i<=n;i++)
+	{
+		p=n;
+		for(int j=i;j<=n&&p>flag;j++)
+		{
+			arr[p][j]=num++;
+			p--;
+		}
+		flag++;
+	}
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n;j++)
+		{
+			System.out.print(arr[i][j]+" ");
+		}
+		System.out.println();
+	}
 	}
 
 }
