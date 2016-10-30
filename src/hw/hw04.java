fa5f3f616da3701217e32842c1d98590f3c94a08
Yingyin <105791120@live.asia.edu.tw>
2016-10-30 15:03:22 +0800

    hw04 105791120 杨钰莹

--- src/hw/hw04.java
+++ src/hw/hw04.java
@@ -1,10 +1,19 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner sca = new Scanner(System.in);
+	double sum=0;
+	int n=sca.nextInt();
+	for(int i=n;i>=1;i++)
+	{
+		sum+=1.0/((2*i-1)*2*i);
+		
+	}
+	System.out.println(sum);
 	}
 
 }
